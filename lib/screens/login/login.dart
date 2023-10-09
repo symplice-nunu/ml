a2be189712df1bac1888c23845ce1c0075444dfd
intwarisymplice@gmail.com <51247342+rukundorkay@users.noreply.github.com>
2023-10-09 17:21:32 +0200

    feat: create sign in inpu fields and button

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,46 +1,89 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            // Display the image at the top
-            Image.asset(
-              'assets/images/login.png',
-            ),
-            SizedBox(height: 16.0),
-            Padding(padding: EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                TextField(
-              decoration: InputDecoration(
-                labelText: 'Username',
+      body: SingleChildScrollView( // Wrap with SingleChildScrollView
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              // Display the image at the top
+              Image.asset(
+                'assets/images/login.png',
               ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
+              Text(
+                'Sign In',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 27,
+                  fontFamily: 'Times New Roman',
+                ),
               ),
-            ),
-            SizedBox(height: 32.0),
-            ElevatedButton(
-              onPressed: () {
-                // Implement your login logic here
-                // For simplicity, let's just print a message
-                print('Login Button Pressed');
-              },
-              child: Text('Login'),
-            ),
-              ],
-            ),
-            )
-          ],
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    // ignore: prefer_const_constructors
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        labelStyle: TextStyle(color: Colors.teal[200]),
+                        prefixIcon: Icon(
+                          Icons.email_outlined,
+                          color: Colors.teal[200],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: TextStyle(color: Colors.teal[200]),
+                        prefixIcon: Icon(
+                          Icons.lock_outline,
+                          color: Colors.teal[200],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 32.0),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0),
+                        child: ElevatedButton(
+                      onPressed: () {
+                        print('Login Button Pressed');
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.teal.shade200),
+                        minimumSize: MaterialStateProperty.all(Size(double.infinity, 50)),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0), // Adjust the value for the desired rounded corners
+                          ),
+                        ),
+                      ),
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
+      ),
     );
   }
 }
